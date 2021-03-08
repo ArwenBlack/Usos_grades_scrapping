bc0cea1326bf701b2dce3e151324348acef981f8
ArwenBlack <arwenamarenwen@gmail.com>
2021-03-08 23:33:19 +0100

    gardes nad database

---
+++ USOS_grades/Database.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, query
+
+engine = create_engine('sqlite:///usos_database.db', echo=True)
+Base = declarative_base()
+
+class Grades_data(Base):
+    __tablename__  = 'class_semester'
+    class_name = Column(String, primary_key=True)
+    semester = Column(String)
+    wyk = Column(Float)
+    cw = Column(Float)
+    lab = Column(Float)
+    pp = Column(Float)
+
+
+Base.metadata.create_all(engine)
+
+def insert_class (class_names, semesters):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(Grades_data.class_name).filter(Grades_data.class_name==class_names)
+    if (not session.query(q.exists()).scalar()):
+        classes = Grades_data(class_name=class_names, semester=semesters)
+        session.add(classes)
+        session.commit()
+
--- USOS_grades/main.py
+++ USOS_grades/main.py
@@ -1,21 +1,34 @@
 from selenium import webdriver
+from Database import *
 import re
 option = webdriver.ChromeOptions()
-#option.add_argument('headless')
+option.add_argument('headless')
 driver = webdriver.Chrome('chromedriver', options=option)
+
+class class_info():
+
+    def __init__(self, name = null, wyk = null, cw = null, lab = null, pp = null):
+        self.Name = name
+        self.WYK = wyk
+        self.CW = cw
+        self.LAB = lab
+        self.PP = pp
+
+
 def login():
     driver.get('https://logowanie.wat.edu.pl/cas/login?service=https%3A%2F%2Fusos.wat.edu.pl%2Fkontroler.php%3F_action%3Dlogowaniecas%2Findex&locale=pl')
-    driver.find_element_by_id('username').send_keys('mail')
-    driver.find_element_by_id('password').send_keys('haslo')
+    driver.find_element_by_id('username').send_keys('')
+    driver.find_element_by_id('password').send_keys('')
     driver.find_element_by_name('submit').click()
 
 
 
-def read_class_names():
+def read_class_names(semester):
     class_table = driver.find_element_by_xpath("//*[@id='layout-c22a']/div[2]/table[2]")
     class_table_source = class_table.get_attribute('outerHTML')
     class_names = re.findall(r'tabindex="0"><span>(.+)</span></a>', class_table_source)
-    print(class_names)
+    for name in class_names:
+        insert_class(name, semester)
 
 
 
@@ -23,65 +36,33 @@ def grades():
     driver.find_element_by_xpath("//*[@id='layout-c12-t']/div[2]/div/nav/ul/li[4]/a").click()
     driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table/tbody/tr[3]/td[4]/a").click()
     list = driver.find_elements_by_xpath("//*[contains(text(),'szczegóły')]")
-    print(len(list))
+    licznik = 1
     for i in range(len(list)):
         list1 = driver.find_elements_by_xpath("//*[contains(text(),'szczegóły')]")
         list1[i].click()
-        read_class_names()
+        read_class_names(licznik)
+        licznik+=1
         driver.back()
 
-
-
-    #do ocen
-    # driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table/tbody/tr[2]/td[2]/a").click()
-    # grade_table = driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table[3]")
-    # grade_table_source = grade_table.get_attribute('outerHTML')
-
-    #driver.close()
+    driver.back()
+    driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table/tbody/tr[2]/td[2]/a").click()
+    table = driver.find_elements_by_xpath("//*[@id='layout-c22a']/div/table[3]/tbody")
+    names = []
+    grades = []
+    for i in table:
+        table_h = i.get_attribute("outerHTML")
+        names += re.findall(r'tabindex="0">(.+)</a>', table_h )
+        grades += re.findall(r' ">(.+)</span>', table_h )
+    # name = re.findall(r'tabindex="0">(.+)</a>', mmclass)
+    # grades = re.findall(r' ">(.+)</span>', mmclass)
+    # print(mmclass)
+    print(names)
+    print(grades)
+    #zeby okienko sie nie pojawiało
+    driver.close()
     # s = "abc123AUG|GAC|UGAasdfg789"
     # substring = re.findall("AUG(.*?)UGA", s)
 
 login()
 grades()
 
-
-# class UsosBot:
-#
-#     def __init__(self, username, password):
-#         self.driver = webdriver.Chrome()  #path to chromedriver
-#         self.driver.get('https://usos.wat.edu.pl/kontroler.php?_action=home/index')
-#         self.driver.find_element_by_xpath("//a[contains(text(), 'zaloguj się')]").click()
-#         self.driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
-#         self.driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
-#         self.driver.find_element_by_xpath("//*[@id='fm1']/section[5]/input[4]").click()
-#
-#     def get_classes(self):
-#         self.driver.find_element_by_xpath("//*[@id='layout-c12-t']/div[2]/div/nav/ul/li[4]/a").click() #'dla studentow' tab
-#         self.driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table/tbody/tr[2]/td[2]/a").click() #'oceny' tab
-#         grades_box = self.driver.find_element_by_xpath("//*[@id='layout-c22a']/div/table[3]")
-#         grades_box_source = grades_box.get_attribute("outerHTML")
-#         class_names = re.findall(r'tabindex="0">(.+)</a>', grades_box_source)
-#         grades = re.findall(r'(\(brak ocen\)|\d,\d|ZAL)</span>', grades_box_source)
-#         return {class_name: grade for class_name, grade in zip(class_names, grades)}
-#
-#     def log_out(self):
-#         self.driver.find_element_by_xpath("//a[contains(text(), 'wyloguj się')]").click()
-#         self.driver.close()
-#
-#
-# def average_grade(classes):
-#     grades = [float(grade.replace(',', '.')) for grade in classes.values() if bool(re.fullmatch(r'\d,\d', grade))]
-#     return sum(grades)/len(grades)
-#
-#
-# index_number = 'weronika.gramacka@student.wat.edu.pl'
-# password = 'Awima454545'
-#
-# my_bot = UsosBot(index_number, password)
-# classes = my_bot.get_classes()
-# my_bot.log_out()
-# avg_grade = average_grade(classes)
-#
-# for class_name, grade in classes.items():
-#     print(f'{class_name} : {grade}')
-# print(f'average grade: {avg_grade}')
